953c03a15d3b5436c14a22e2630c2c786c2d573c
lordennies <lordennies@gmail.com>
2018-08-12 18:34:59 +0700

    Update api for uploading image to server

--- dev/application/controllers/Api.php
+++ dev/application/controllers/Api.php
@@ -18,8 +18,7 @@ class Api extends CI_Controller {
 		$user = $this->User_model->get_by(array('email' => @$post['email'], 'group' => 'user'), 1, null, true);
 
 		if (@$user->password == crypt(@$post['password'], @$user->password)) {
-			$response = array('status' => 'success', 'user_id' => $user->user_id, 
-					'username' => $user->username, 'email' => $user->email, 'has_made_req' => $user->has_made_req);
+			$response = array('status' => 'success', 'user_id' => $user->user_id, 'email' => $user->email);
 			echo json_encode($response);
 		} else {
 			$response = array('status' => 'failed', 'message' => 'Email atau password anda salah.');
@@ -49,9 +48,10 @@ class Api extends CI_Controller {
 				);
 				/* insert data ke tabel peminjaman dan update field has_made_req di tabel user */
 				if ($this->Peminjaman_model->insert($data)) {
+					$insert_id = $this->db->insert_id();
 					$has_made_req = array('has_made_req' => 1);
 					$this->User_model->update($has_made_req, array('user_id' => $post['user_id']));
-					array_push($response, array('status' => 'success'));
+					array_push($response, array('status' => 'success', 'peminjaman_id' => $insert_id));
 				} else {
 					array_push($response, array('status' => 'failed'));
 				}
@@ -87,6 +87,48 @@ class Api extends CI_Controller {
 			echo json_encode(array('status' => 'able'));
 		} else {
 			echo json_encode(array('status' => 'unable'));
+		}
+	}
+
+	public function cekStatusPermohonan() {
+		global $SConfig;
+		$this->load->model(array('User_model', 'Peminjaman_model'));
+
+		header('Access-Control-Allow-Origin: *');
+		header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+
+		$post = $this->input->post(null, true);
+		$peminjaman = $this->Peminjaman_model->get($post['peminjaman_id']);
+
+		if ($peminjaman->status_req == 0) {
+			echo json_encode(array('status' => 'pending'));
+		} else if ($peminjaman->status_req == 1) {
+			echo json_encode(array('status' => 'diizinkan'));
+		} else {
+			$has_made_req = array('has_made_req' => 0);
+			$this->User_model->update($has_made_req, array('user_id' => $post['user_id']));
+			echo json_encode(array('status' => 'ditolak'));
+		}
+	}
+
+	public function upload() {
+		$post = $this->input->post(null, true);
+		$image = $post["image"];
+		if ($image) {
+			$target_dir = "uploads";
+
+			if (!file_exists($target_dir)) {
+				mkdir($target_dir);
+			}
+
+			$target_dir = $target_dir."/".rand()."_".time().".jpeg";
+			if (file_put_contents($target_dir, base64_decode($image))) {
+				echo json_encode(array('response' => 'Image uploaded successfully'));
+			} else {
+				echo json_encode(array('response' => 'Image upload failed'));
+			}
+		} else {
+			echo json_encode(array('response' => 'Image upload failed'));
 		}
 	}
 
